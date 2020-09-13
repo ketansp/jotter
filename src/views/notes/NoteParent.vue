5bfd019368c99744731c3f0fe3807b0f3197727a
Ketan Patil <100.percent.ketan@gmail.com>
2020-09-13 15:37:21 +0530

    adding pluggins for block editor

--- src/views/notes/NoteParent.vue
+++ src/views/notes/NoteParent.vue
@@ -20,6 +20,19 @@
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import EditorJS from "../../../node_modules/@editorjs/editorjs";
+const Header = require("@editorjs/header");
+const CodeTool = require("@editorjs/code");
+const Delimiter = require("@editorjs/delimiter");
+const Checklist = require("@editorjs/checklist");
+const List = require("@editorjs/list");
+const Quote = require("@editorjs/quote");
+const SimpleImage = require("@editorjs/simple-image");
+const Table = require("@editorjs/table");
+const InlineCode = require("@editorjs/inline-code");
+const Paragraph = require("@editorjs/paragraph");
+const Marker = require("@editorjs/marker");
+const Underline = require("@editorjs/underline");
+const Warning = require("@editorjs/warning");
 
 @Component({
   components: {}
@@ -29,7 +42,69 @@ export default class NoteParent extends Vue {
     /**
      * Id of Element that should contain Editor instance
      */
-    holder: "editor"
+    holder: "editor",
+    autofocus: true,
+    //data: existingData,
+    tools: {
+      header: {
+        class: Header,
+        config: {
+          placeholder: "Enter a header",
+          levels: [1, 2, 3, 4, 5, 6],
+          defaultLevel: 3
+        }
+      },
+      code: CodeTool,
+      delimiter: Delimiter,
+      checklist: {
+        class: Checklist,
+        inlineToolbar: true
+      },
+      list: {
+        class: List,
+        inlineToolbar: true
+      },
+      quote: {
+        class: Quote,
+        inlineToolbar: true,
+        shortcut: "CMD+SHIFT+O",
+        config: {
+          quotePlaceholder: "Enter a quote",
+          captionPlaceholder: "Quote's author"
+        }
+      },
+      image: SimpleImage,
+      table: {
+        class: Table,
+        inlineToolbar: true,
+        config: {
+          rows: 5,
+          cols: 4
+        }
+      },
+      inlineCode: {
+        class: InlineCode,
+        shortcut: "CMD+SHIFT+M"
+      },
+      paragraph: {
+        class: Paragraph,
+        inlineToolbar: true
+      },
+      warning: {
+        class: Warning,
+        inlineToolbar: true,
+        shortcut: "CMD+SHIFT+W",
+        config: {
+          titlePlaceholder: "Title",
+          messagePlaceholder: "Message"
+        }
+      },
+      Marker: {
+        class: Marker,
+        shortcut: "CMD+SHIFT+M"
+      },
+      underline: Underline
+    }
   });
 }
 </script>
